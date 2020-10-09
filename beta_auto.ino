9723637384e4b22639d567488a6fa0f14e65394c
rumburaq <60288604+rumburaq@users.noreply.github.com>
2020-10-09 18:49:57 +0200

    update beta_auto.ino - pouziti millis() časovače

--- beta_auto.ino
+++ beta_auto.ino
@@ -20,27 +20,34 @@ const int echoPinZ = A3;
 int vz; //vzdalenost (stred)
 int vzR;//vzdalenost prava...
 int vzL;
-int vzZ; //(zadni)
+extern int vzZ; //(zadni) !!!
 int minvz = 30; //(ne)bude se mněnit a muze se dat na #define
 int minvzS = 30; //min. vzdálenost strana 30cm
+int minvzZ = 30;
 long duration; //cas za ktery letel zvuk od senzoru a zpet (stred) senzor
 long durationR; //pravy senzor...
 long durationL;
 long durationZ;
+unsigned long perv = 0; //pervious time 
+unsigned long now = 0;
+int interval = 100; //#define ?
+  
 
 void setup() {
-  pinMode(RELE_PIN_1, OUTPUT); //                                  IN1    IN2  vysledek               
+  digitalWrite(RELE_PIN_1, LOW); // low = zapne se //
+  digitalWrite(RELE_PIN_2, HIGH); //  } = 1 0
+  pinMode(RELE_PIN_1, OUTPUT); //                                  IN1    IN2  vysledek
   pinMode(RELE_PIN_2, OUTPUT);//                                    1      1   motor se otaci 1 smerem
-// je to tady protoze je to (asi) rychlejsi nez funkce //           1      0   motor stoji
-  digitalWrite(RELE_PIN_1, LOW); // low = zapne se //               0      1   motor stoji
-  digitalWrite(RELE_PIN_2, HIGH); //  } = 1 0                       0      0   motor jede na 2 stranu
+  // je to tady protoze je to (asi) rychlejsi nez funkce //           1      0   motor stoji
+  //digitalWrite(RELE_PIN_1, LOW); // low = zapne se //             0      1   motor stoji
+  //digitalWrite(RELE_PIN_2, HIGH); //  } = 1 0                     0      0   motor jede na 2 stranu
 
   pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT);
-  
+
   pinMode(trigPinR, OUTPUT);
   pinMode(echoPinR, INPUT);
-  
+
   pinMode(trigPinL, OUTPUT);
   pinMode(echoPinL, INPUT);
 
@@ -52,36 +59,40 @@ void setup() {
   motor.setSpeed(120);
   motor.run(RELEASE);
 
+  delay(500);// aby se to nacetlo
 }
 
 void loop() {
-  sensorsRead(); //nacti hodnoty ze senzorů
-  if (vz < minvz) || (vzR < minvzS) || (vzL < minvzS) {
-  
-  if (vz < minvz) && (vzR < minvzS) && (vzL < minvzS) { //pokud jsi blízko prekazce zastav
-    pause();
-    //otocka();
+  now = millis();
+  if (now - perv > interval){ 
+    perv = now;
+    sensorsRead(); // nemuze tady bejt?
   }
+  if ((vz < minvz) || (vzR < minvzS) || (vzL < minvzS)) { //pridat > 2 aby nekolildovalu s nulou
 
-  else if (vzR < vzL){ //kdyz je na levo víc místa jed do leva
-    left();
+    if ((vz < minvz) && (vzR < minvzS) && (vzL < minvzS)) {
+      pause();
+      //otocka();
+    }
+
+    else if (vzR < vzL) {
+      left();
+    }
+
+    else if (vzR > vzL) {
+      right();
+    }
+
+    else {
+      go();
+    }
   }
-
-  else if (vzR > vzL){
-    right();
-  }
-
   else {
     go();
   }
-}
-  else {
-    go();
-  }
-}
 
-//tahle funkce zmeri vzdálenost u 3 prednich senzoru v cm
-void sensorsRead(){ // nesmi to bejt void OPRAVIT? 
+}
+void sensorsRead() { 
   // mereni 30-40 tisicin sekundy
   //front sensor
   
@@ -92,15 +103,15 @@ void sensorsRead(){ // nesmi to bejt void OPRAVIT?
   digitalWrite(trigPin, HIGH);
   delayMicroseconds(10);
   digitalWrite(trigPin, LOW);
-  duration = pulseIn(echoPin, HIGH); // pulseIn zmeri cas v mikrosedundach jak dlouho zvukova vlna letela 
-  vz = duration * 0.034 / 2; // vz = vzdálenost | s = t * v | s = duration * 0.043cm/µs |
+  duration = pulseIn(echoPin, HIGH); // pulseIn zmeri cas v mikrosedundach jak dlouho zvukova vlna letela |//!? MOZNA PREJMENOVAT DURATION NA TIME
+  vz = duration * 0.034 / 2; // vz = vzdálenost | s = t * v | s = duration * 0.043cm/micros |
   Serial.println("stred 1: ");
   Serial.println(vz);
-  //right sensor 
-  
+  //right sensor
+
   digitalWrite(trigPinR, LOW);
   delayMicroseconds(2);
-  
+
   digitalWrite(trigPinR, HIGH);
   delayMicroseconds(10);
   digitalWrite(trigPin, LOW);
@@ -108,11 +119,11 @@ void sensorsRead(){ // nesmi to bejt void OPRAVIT?
   vzR = durationR * 0.034 / 2; //
   Serial.println("pravej 2: ");
   Serial.println(vzR);
-  //left sensor 
-  
+  //left sensor
+
   digitalWrite(trigPinL, LOW);
   delayMicroseconds(2);
-  
+
   digitalWrite(trigPinL, HIGH);
   delayMicroseconds(10);
   digitalWrite(trigPinL, LOW);
@@ -120,32 +131,30 @@ void sensorsRead(){ // nesmi to bejt void OPRAVIT?
   vzL = durationL * 0.034 / 2; //
   Serial.println("levej 3: ");
   Serial.println(vzL);
-
-  delay(100); // 10 mereni /s (za sekundu) ! zmennit na pouzití munkce milis();
-
-  //back sensor 
+  //delay(100);// 10 mereni  (za sekundu) ZRUSIT
   
+  
+
+  //back sensor
+
   /*digitalWrite(trigPinZ, LOW);
-  delayMicroseconds(2);
-  
-  digitalWrite(trigPinZ, HIGH); // zatim nemazat
-  delayMicroseconds(10);
-  digitalWrite(trigPinZ, LOW);
-  durationZ = pulseIn(echoPinZ, HIGH);
-  vzZ = durationZ * 0.034 / 2; //
-  Serial.println("senzor Zadni____: ");
-  Serial.println(vzZ);*/
-  
- }
-  //zmeri vzdalenost v cm na zadnim senzoru
-  //tahle funkce je oddelena od sensorsRead protoze
-  //nechci merit vzdalenost za autem kdyz jedu dopredu
-  void backSensor(){ // ne na void?
-  //back sensor 
-  
+    delayMicroseconds(2);
+
+    digitalWrite(trigPinZ, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(trigPinZ, LOW);
+    durationZ = pulseIn(echoPinZ, HIGH);
+    vzZ = durationZ * 0.034 / 2; //
+    Serial.println("senzor Zadni____: ");
+    Serial.println(vzZ);*/
+}
+
+void backSensor() {
+  //back sensor
+
   digitalWrite(trigPinZ, LOW);
   delayMicroseconds(2);
-  
+
   digitalWrite(trigPinZ, HIGH);
   delayMicroseconds(10);
   digitalWrite(trigPinZ, LOW);
@@ -153,67 +162,67 @@ void sensorsRead(){ // nesmi to bejt void OPRAVIT?
   vzZ = durationZ * 0.034 / 2; //
   Serial.println("senzor Zadni____: ");
   Serial.println(vzZ);
-  delay(100); // 10 mereni /s (za sekundu)
-  
-  }
+  // delay(100); // 10 mereni /s (za sekundu)
 
-  void go(){
-  digitalWrite(RELE_PIN_1, HIGH);              
+}
+
+void go() {
+  digitalWrite(RELE_PIN_1, HIGH);
   digitalWrite(RELE_PIN_2, HIGH); //} 1 1
-  }
+}
 
-  void pause(){
-    digitalWrite(RELE_PIN_1, LOW);              
-    digitalWrite(RELE_PIN_2, HIGH); //} 0 1
-  }
+void pause() {
+  digitalWrite(RELE_PIN_1, LOW);
+  digitalWrite(RELE_PIN_2, HIGH); //} 0 1
+}
 
-  void left(){
-    motor.setSpeed(120); //nemelo by se tady nechat
-    motor.run(BACKWARD);
-  }
+void left() {
+  motor.setSpeed(120); //nemelo by se tady nechat
+  motor.run(BACKWARD);
+}
 
-  void right(){
-    motor.run(FORWARD);
-  }
+void right() {
+  motor.run(FORWARD);
+}
 
-  void backward(){
-    digitalWrite(RELE_PIN_1, LOW);              
-    digitalWrite(RELE_PIN_2, LOW); //} 0 0
-  }
+void backward() {
+  digitalWrite(RELE_PIN_1, LOW);
+  digitalWrite(RELE_PIN_2, LOW); //} 0 0
+}
 
-  void otocka() { // to radsi ani nikdo nectete ani to zatim nefunguje
-    int leftcount = 0;
-    pause();
-    //delay(2000) //!!??? je horni hranice aby nerozhodila senzory?
-    while (vzZ > minvzZ){ //!! NEKDE POUZIT BACKSENSOR !!
-      if (vzZ > minvzZ && leftcount == 0 ){
-        left();
-        //delay(50) ?
-        backward();
-        leftcount = 1;
-      }
-      else if (vzZ > minvzZ && leftcount == 1){ //vyresit lag senzoru otestovat funkci na zpatecku (v hlave) nastenka napsat funkci fullright a naopak
-        //backward();
-        motor.run(RELEASE);
-      }
-      else if (vzZ > minVZ && leftcount == 1){
-        pause();
-        rightfull(); // musim vytvorit na z leva do prava a obracene 
-        go();
-      }
-      
+void otocka() {
+  int leftcount = 0;
+  pause();
+  //delay(2000) //!!??? je horni hranice aby nerozhodila senzory?
+  while (vzZ > minvzZ) { //!! NEKDE POUZIT BACKSENSOR !!
+    if (vzZ > minvzZ && leftcount == 0 ) {
+      left();
+      //delay(50) ?
+      backward();
+      leftcount = 1;
     }
-    
+    else if (vzZ > minvzZ && leftcount == 1) { //vyresit lag senzoru otestovat funkci na zpatecku (v hlave)
+      //backward();
+      motor.run(RELEASE);
+    }
+    else if (vzZ > minvz && leftcount == 1) {
+      pause();
+      fullright(); // musim vytvorit na z leva do prava a obracene
+      go();
+    }
+
   }
 
-  void fullright() { //mozna k nicemu
-    motor.run(FORWARD);
-    delay(50); //mozna vic mozna min
-    motor.run(RELEASE);
-  }
+}
 
-  void fullleft() {
-    motor.run(BACKWARD); 
-    delay(50); //mozna vic mozna min mozna pouzit milis misto delay
-    motor.run(RELEASE);
-  }
+void fullright() {
+  motor.run(FORWARD);
+  delay(50); //mozna vic mozna min
+  motor.run(RELEASE);
+}
+
+void fullleft() {
+  motor.run(BACKWARD);
+  delay(50); //mozna vic mozna min
+  motor.run(RELEASE);
+}
